ccb2a2c930ec4942fa272368fcdce927506439bf
Alejandro Exojo <alex@disperso.net>
2023-08-01 18:59:33 +0200

    Rework what is shown on the OLED displays

--- keyboards/sofle/keymaps/suy/keymap.c
+++ keyboards/sofle/keymaps/suy/keymap.c
@@ -172,49 +172,43 @@ static void render_logo(void) {
 }
 
 static void print_status_narrow(void) {
-    // Print current mode
-    oled_write_P(PSTR("\n\n"), false);
+    // TODO: remove eventually. I don't think I'll ever change this mode.
     oled_write_ln_P(PSTR("MODE"), false);
     oled_write_ln_P(PSTR(""), false);
-    if (keymap_config.swap_lctl_lgui) {
-        oled_write_ln_P(PSTR("MAC"), false);
+    if (!keymap_config.swap_lctl_lgui) {
+        oled_write_ln_P(PSTR("Win"), false);
     } else {
-        oled_write_ln_P(PSTR("WIN"), false);
+        oled_write_ln_P(PSTR("Mac"), false);
     }
 
-    switch (get_highest_layer(default_layer_state)) {
-        case _QWERTY:
-            oled_write_ln_P(PSTR("Qwrt"), false);
-            break;
-        case _COLEMAK:
-            oled_write_ln_P(PSTR("Clmk"), false);
-            break;
-        default:
-            oled_write_P(PSTR("Undef"), false);
-    }
     oled_write_P(PSTR("\n\n"), false);
-    // Print current layer
-    oled_write_ln_P(PSTR("LAYER"), false);
+
+    oled_write_ln_P(PSTR("LYER"), false);
+    oled_write_ln_P(PSTR(""), false);
     switch (get_highest_layer(layer_state)) {
         case _COLEMAK:
         case _QWERTY:
-            oled_write_P(PSTR("Base\n"), false);
+            oled_write_ln_P(PSTR("Base"), false);
             break;
         case _RAISE:
-            oled_write_P(PSTR("Raise"), false);
+            oled_write_ln_P(PSTR("Raise"), false);
             break;
         case _LOWER:
-            oled_write_P(PSTR("Lower"), false);
+            oled_write_ln_P(PSTR("Lower"), false);
             break;
         case _ADJUST:
-            oled_write_P(PSTR("Adj\n"), false);
+            oled_write_ln_P(PSTR("Adjst"), false);
             break;
         default:
             oled_write_ln_P(PSTR("Undef"), false);
     }
     oled_write_P(PSTR("\n\n"), false);
     led_t led_usb_state = host_keyboard_led_state();
-    oled_write_ln_P(PSTR("CPSLK"), led_usb_state.caps_lock);
+    if (led_usb_state.caps_lock) {
+        oled_write_ln_P(PSTR("CAPS LOCK!"), false);
+    } else { // Needed to clear the previous caps lock message
+        oled_write_P(PSTR("\n\n"), false);
+    }
 }
 
 oled_rotation_t oled_init_user(oled_rotation_t rotation) {
