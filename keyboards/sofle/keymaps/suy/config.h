b393c994ee84d2527a6c2aa60cfeabcc5b770b71
Alejandro Exojo <alex@disperso.net>
2023-08-01 17:00:08 +0200

    Reduce ONESHOT_TIMEOUT

--- keyboards/sofle/keymaps/suy/config.h
+++ keyboards/sofle/keymaps/suy/config.h
@@ -9,4 +9,4 @@ for more options.
 #define TAPPING_TERM 260
 
 #define ONESHOT_TAP_TOGGLE 5  /* Tapping this number of times holds the key until tapped once again. */
-#define ONESHOT_TIMEOUT 5000  /* Time (in ms) before the one shot key is released */
+#define ONESHOT_TIMEOUT 4000  /* Time (in ms) before the one shot key is released */
