a3b900f93ba88750460fe4b4637494fab9e89af3
Alejandro Exojo <alex@disperso.net>
2023-08-06 23:23:23 +0200

    Swap Space and Enter again

--- keyboards/sofle/keymaps/suy/keymap.c
+++ keyboards/sofle/keymaps/suy/keymap.c
@@ -28,7 +28,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|  MUTE |    |       |------+------+------+------+------+------|
  * |LShift|   Z  |   X  |   C  |   V  |   B  |-------|    |-------|   N  |   M  |   ,  |   .  |   /  |RShift|
  * `-----------------------------------------/       /     \      \-----------------------------------------'
- *            | LCTR | LGUI | LAlt |LOWER | /Space  /       \Enter \  |RAISE | RCTR | RAlt | RGUI |
+ *            | LCTR | LGUI | LAlt |LOWER | /Enter  /       \Space \  |RAISE | RCTR | RAlt | RGUI |
  *            |      |      |      |      |/       /         \      \ |      |      |      |      |
  *             `----------------------------------'           '------''---------------------------'
  */
@@ -66,7 +66,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                      KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
   KC_ESC,  DUAL_A,  DUAL_S,  DUAL_D,  DUAL_F,  KC_G,                      KC_H,    DUAL_J,  DUAL_K,  DUAL_L,  DUAL__,  KC_QUOT,
   OSMS,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_MUTE, XXXXXXX, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, OSMR,
-                    OSMC,    OSMG,    OSMA,    LOWERL,  KC_SPC,  KC_ENT,  RAISEL,  KC_RCTL, KC_RALT, KC_RGUI
+                    OSMC,    OSMG,    OSMA,    LOWERL,  KC_ENT,  KC_SPC,  RAISEL,  KC_RCTL, KC_RALT, KC_RGUI
 ),
 
 
